d4eaf9255a607d0f589f11e38d231ca7d5b4e24e
Maro <marolsw.twitch@gmail.com>
2026-07-23 13:27:55 +0200

    fix: remove hardcoded height from phone app layout

--- src/components/apps/PhoneApp.vue
+++ src/components/apps/PhoneApp.vue
@@ -54,7 +54,7 @@ import { Clock, Search } from '@lucide/vue';
         display: flex;
         flex-direction: column;
         width: 100%;
-        height: 82%;
+        // height: 82%;
         margin-top: 1.5cqh;
 
         .input-group {
