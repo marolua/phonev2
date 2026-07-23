a20199e217542210728564affba3aa4b27fb6337
Maro <marolsw.twitch@gmail.com>
2026-07-23 13:01:58 +0200

    feat(phone-app): add recent call container

--- src/components/apps/PhoneApp.vue
+++ src/components/apps/PhoneApp.vue
@@ -6,10 +6,15 @@ import { Clock, Search } from '@lucide/vue';
     <div class="phone-app">
         <span class="title">Recent</span>
         <div class="container">
-
             <div class="input-group">
                 <Search class="search-icon" color="rgb(255, 255, 255, 0.8)" :size="20" />
                 <input type="text" placeholder="Rechercher">
+            </div>
+
+            <div class="container-recent-call">
+                <div class="recent-call">
+
+                </div>
             </div>
         </div>
         <div class="bottom-app-phone">
@@ -39,9 +44,9 @@ import { Clock, Search } from '@lucide/vue';
         display: flex;
         flex-direction: column;
         width: 100%;
-        height: 75%;
+        height: 82%;
         margin-top: 1.5cqh;
-        // background-color: blue;
+        background-color: blue;
 
         .input-group {
             position: relative;
@@ -73,12 +78,28 @@ import { Clock, Search } from '@lucide/vue';
             }
         }
 
+        .container-recent-call {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 100%;
+            max-height: 92.5%;
+            background-color: green;
+
+            .recent-call {
+                display: flex;
+                width: 100%;
+                height: 8cqh;
+                background-color: aqua;
+            }
+        }
+
     }
 
     .bottom-app-phone {
         display: flex;
         width: 100%;
-        height: 20%;
+        height: 18%;
         background-color: red;
     }
 }
