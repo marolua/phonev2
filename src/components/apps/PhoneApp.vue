4fa6aee950a32008b67e5c94d17b2683987f6cb1
Maro <marolsw.twitch@gmail.com>
2026-07-24 22:13:07 +0200

    fix(components/apps/phoneapp): remove box shadow and add font family

--- src/components/apps/PhoneApp.vue
+++ src/components/apps/PhoneApp.vue
@@ -345,10 +345,6 @@ const removeLastDigit = () => {
             height: 68%;
             border-radius: 6cqh;
             background-color: rgba(51, 51, 51, 0.3);
-            // box-shadow:
-            //     0 10px 26px rgba(0, 0, 0, 0.2),
-            //     inset 0 1px 0 rgba(255, 255, 255, 0.85),
-            //     inset 0 -6px 10px -6px rgba(0, 0, 0, 0.15);
 
             .categorie {
                 display: flex;
@@ -359,6 +355,7 @@ const removeLastDigit = () => {
                 width: 30%;
                 height: 90%;
                 border-radius: 5.7cqh;
+                font-family: "SF Pro";
                 transition: all ease-in-out 0.2s;
                 font-size: 1.6cqh;
                 font-weight: 500;
