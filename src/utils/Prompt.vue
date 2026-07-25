337d3182bde1505aec94b42cf7611101af155dfb
Maro <marolsw.twitch@gmail.com>
2026-07-25 21:56:35 +0200

    fix: update prompt component styles

--- src/utils/Prompt.vue
+++ src/utils/Prompt.vue
@@ -15,9 +15,9 @@
                 <span class="description">Are you sure you want to delete this conversation?</span>
             </div>
             <div class="bottom-prompt-without">
-                <span class="button-prompt">Accept</span>
+                <span class="button-prompt">Cancel</span>
                 <span class="bar"></span>
-                <span class="button-prompt">Decline</span>
+                <span class="button-prompt">Delete</span>
             </div>
         </div>
     </div>
@@ -42,11 +42,11 @@
         display: flex;
         flex-direction: column;
         border-radius: 3.5cqw;
-        width: 70%;
+        width: 96%;
         box-sizing: border-box;
-        padding: 2cqw 2cqw 3cqw 2cqw;
-        height: 20%;
-        background-color: rgba(15, 15, 15, 0.9);
+        height: 18%;
+        overflow: hidden;
+        background-color: rgba(40, 40, 40, 0.97);
 
         .top-prompt-without {
             position: relative;
@@ -54,19 +54,23 @@
             flex-direction: column;
             justify-content: center;
             align-items: center;
+            box-sizing: border-box;
+            padding: 1.5cqh 4cqw 0;
             text-align: center;
-            gap: 0.5cqh;
-            height: 65%;
+            gap: 0.45cqh;
+            height: 64%;
             width: 100%;
             // background-color: green;
 
             .title-prompt {
                 font-size: 2.1cqh;
-                font-weight: 500;
+                font-weight: 600;
+                line-height: 1.1;
             }
 
             .description {
-                font-size: 1.6cqh;
+                font-size: 1.55cqh;
+                line-height: 1.25;
             }
         }
 
@@ -80,9 +84,9 @@
 
             .button-prompt {
                 font-size: 2cqh;
-                font-family: "SF Pro";
+                font-family: "SF Pro Display";
                 font-weight: 500;
-                color: rgb(0, 64, 255);
+                color: rgb(0, 157, 255);
 
                 &:hover {
                     cursor: pointer;
@@ -90,7 +94,7 @@
             }
 
             .bar {
-                width: 0.5cqw;
+                width: 0.35cqw;
                 border-radius: 2cqw;
                 height: 3cqh;
                 background-color: rgba(128, 128, 128, 0.65);
