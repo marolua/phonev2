e6e5fc608070a62d7a158c2214332c4d4cdb471d
Maro <marolsw.twitch@gmail.com>
2026-07-25 21:33:50 +0200

    fix: update prompt background opacity

--- src/utils/Prompt.vue
+++ src/utils/Prompt.vue
@@ -18,7 +18,7 @@
     width: 100%;
     height: 100%;
     border-radius: 13.4cqw;
-    background-color: rgba(255, 255, 255, 0.509);
+    background-color: rgba(255, 255, 255, 0.75);
     filter: drop-shadow(0px -6px 100px #000000);
     z-index: 50;
 }
