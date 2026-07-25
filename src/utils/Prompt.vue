7ff72985faaea94b10f48ca91d62efd13fd96259
Maro <marolsw.twitch@gmail.com>
2026-07-25 22:13:17 +0200

    fix: adjust prompt height to 28%

--- src/utils/Prompt.vue
+++ src/utils/Prompt.vue
@@ -49,7 +49,7 @@
         display: flex;
         flex-direction: column;
         width: 67.5%;
-        height: 30%;
+        height: 28%;
         box-sizing: border-box;
         overflow: hidden;
         border-radius: 3.5cqw;
