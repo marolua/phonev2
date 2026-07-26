133daa17c726cb6580d7b5463ad318e79672531a
Maro <marolsw.twitch@gmail.com>
2026-07-26 15:21:44 +0200

    feat(settings-app): add styles for settings app component

--- src/components/apps/Settings.vue
+++ src/components/apps/Settings.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
+    <div class="settings-app">
 
+    </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.settings-app {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    max-height: 100%;
+    box-sizing: border-box;
+    user-select: none;
+    padding: 15cqw 3cqh 0cqw;
+    background-color: rgb(10, 10, 10);
+}
+</style>
