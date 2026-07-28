5e28c8e1139e7e4c1e5d0fb868698e703ad32387
Maro <marolsw.twitch@gmail.com>
2026-07-28 14:38:19 +0200

    fix: increase hover scale in settings component

--- src/components/apps/Settings.vue
+++ src/components/apps/Settings.vue
@@ -183,7 +183,7 @@ import Inconnu from "../../assets/inconnu.png"
 
             &:hover {
                 cursor: pointer;
-                transform: scale(1.005);
+                transform: scale(1.05);
             }
         }
     }
