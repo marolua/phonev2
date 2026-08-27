d1029c26419ab0b2fd64733ee6436ff3854eb5e3
Maro <marolsw.twitch@gmail.com>
2026-08-27 18:46:36 +0200

    feat: add initial message app structure and styling

--- src/components/apps/Message.vue
+++ src/components/apps/Message.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
+    <div class="message-app">
+        
 
+    </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.message-app { 
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    max-height: 100%;
+    box-sizing: border-box;
+    user-select: none;
+    padding: 15cqw 3cqh 0cqw;
+    background-color: rgb(10, 10, 10);
+}
+</style>
