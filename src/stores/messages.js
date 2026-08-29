aa76191abc3a3a007dde485e352cfe3fbe23aea4
Maro <marolsw.twitch@gmail.com>
2026-08-29 22:10:28 +0200

    style: standardize quotation marks and improve indentation in messages store

--- src/stores/messages.js
+++ src/stores/messages.js
@@ -1,8 +1,7 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 
-// Nombre de messages non lus affiché sur l'icône Messages.
 export const unreadMessageCount = ref(2);
 
 export const markMessagesAsRead = () => {
-    unreadMessageCount.value = 0;
+  unreadMessageCount.value = 0;
 };
